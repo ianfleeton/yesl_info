0c0d01f5885fe2d92ab06b8ab9f53e3336e41bd9
Ian Fleeton <ianfleeton@gmail.com>
2010-05-12 17:16:28 +0100

    Tick and cross helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,30 @@
+# coding: utf-8
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def flash_notice 
     if flash[:notice] 
       content_tag('div', h(flash[:notice]), {:id => "flash_notice"}) 
     end
+  end
+
+  def a_tick
+    '<span class="tick">✔</span>'
+  end
+
+  def a_cross
+    '<span class="cross">✘</span>'
+  end
+
+  def tick_cross yes, show_cross=true
+    if yes
+      a_tick
+    elsif show_cross
+      a_cross
+    end
+  end
+
+  def tick yes
+    tick_cross yes, false
   end
 
   def alpha_links
