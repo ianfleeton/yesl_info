4fcdc206e305a18bbbae5c2ab488378824cde92a
Ian Fleeton <ianfleeton@gmail.com>
2010-05-12 17:09:48 +0100

    Domains
    Create action; update action; handle when domain not found; forwarding, forwarding_from and hosting_accounts relationships; edit view; nslookup; whois; with_us

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,14 +1,14 @@
 class DomainsController < ApplicationController
   before_filter :admin_required
-  before_filter :find_domain, :only => [:show, :destroy]
-  
+  before_filter :find_domain, :except => [:index, :new, :create]
+
   def index
     @domains = Domain.all :order => :name
   end
   
   def show
   end
-  
+
   def new
     @domain = Domain.new
     begin
@@ -19,7 +19,29 @@ class DomainsController < ApplicationController
       redirect_to domains_path
     end
   end
-  
+
+  def create
+    @domain = Domain.new(params[:domain])
+    if @domain.save
+      flash[:notice] = "Added new domain."
+      redirect_to domain_path(@domain)
+    else
+      render :action => "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @domain.update_attributes(params[:domain])
+      flash[:notice] = 'Domain updated.'
+      redirect_to domain_path(@domain)
+    else
+      render :action => 'edit'
+    end
+  end
+
   def destroy
     @domain.destroy
     flash[:notice] = "Domain deleted."
@@ -29,6 +51,10 @@ class DomainsController < ApplicationController
   private
   
   def find_domain
-    @domain = Domain.find(params[:id], :include => :organisation)
+    @domain = Domain.find_by_id(params[:id], :include => :organisation)
+    if @domain.nil?
+      flash[:notice] = "That domain doesn't exist."
+      redirect_to domains_path
+    end
   end
 end
--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,3 +1,6 @@
 class Domain < ActiveRecord::Base
   belongs_to :organisation
+  belongs_to :forwarding, :class_name => 'Domain'
+  has_many :forwarding_from, :class_name => 'Domain', :foreign_key => 'forwarding_id', :order => :name, :dependent => :nullify
+  has_many :hosting_accounts, :order => :host_name
 end
