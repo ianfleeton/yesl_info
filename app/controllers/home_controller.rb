7a5b2c5c32ba75e268c98a9a2ae093c7ec862c3e
Ian Fleeton <ianfleeton@gmail.com>
2010-05-12 17:14:18 +0100

    Home page and passwords page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,18 @@
+class HomeController < ApplicationController
+  def index
+  end
+  
+  def passwords
+    @passwords = Array.new
+    10.times {@passwords << create_password}
+  end
+  
+  private
+  
+  def create_password
+    chars = %w{a b c d e f g h j k m n o p q r s t u v w x y z A B C D E F G H J K L M N P Q R S T U V W X Y Z 2 3 4 5 6 7 8 9}
+    pw = ''
+    10.times {pw += chars[rand(chars.length)]}
+    pw
+  end
+end
