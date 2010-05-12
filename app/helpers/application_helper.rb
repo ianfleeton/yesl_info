2c0124b4578e210c7387ac856c9d2a7848798b6e
Ian Fleeton <ianfleeton@gmail.com>
2010-05-12 17:16:40 +0100

    Date and time formatting helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,4 +35,38 @@ module ApplicationHelper
     end
     html += '</ul>'
   end
+
+  def format_date d
+    return 'Unknown' if d.nil?
+    t = d.to_time
+    if t.today?
+      'Today'
+    elsif (t+1.day).today?
+      'Yesterday'
+    elsif (Time.now - t) < 1.week
+      t.strftime('%A')
+    elsif t.year == Time.now.year
+      t.strftime("%e %b")
+    else
+      t.strftime("%e %b %Y")
+    end
+  end
+
+  def format_time t
+    if (Time.now - t) < 2.minutes
+      'Just a moment ago'
+    elsif (Time.now - t) < 1.hours
+      ((Time.now - t)/60).ceil.to_s + ' minutes ago'
+    elsif t.today?
+      'Today at ' + t.strftime("%l:%M%p").downcase
+    elsif (t+1.day).today?
+      'Yesterday at ' + t.strftime("%l:%M%p").downcase
+    elsif (Time.now - t) < 1.week
+      t.strftime('%A at ') + t.strftime("%l:%M%p").downcase
+    elsif t.year == Time.now.year
+      t.strftime("%e %b at ") + t.strftime("%l:%M%p").downcase
+    else
+      t.strftime("%e %b %Y at ") + t.strftime("%l:%M%p").downcase
+    end
+  end
 end
