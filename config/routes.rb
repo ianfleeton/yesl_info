bb2569b23d849a7e78fb00e9ee20102a03cc2331
Ian Fleeton <ianfleeton@gmail.com>
2010-09-29 19:42:39 +0100

    Update routes
    For home page, addresses, domains,
    hosting_accounts, note_pads, timesheet_entries
    and users

--- config/routes.rb
+++ config/routes.rb
@@ -31,15 +31,20 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "home"
 
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
-  map.resources :domains
+  map.resources :addresses
+  map.resources :domains, :collection => {:whois => :get}
+  map.resources :hosting_accounts, :collection => {:backups => :get, :backed_up => :get}
+  map.resources :note_pads
   map.resources :organisations
+  map.resources :timesheet_entries
+  map.resources :users
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
