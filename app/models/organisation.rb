cc7b84a3adc36dd5571c805710b45c4195a7d1e5
Ian Fleeton <ianfleeton@gmail.com>
2010-09-29 19:41:17 +0100

    More associations for Organisation

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,4 +1,8 @@
 class Organisation < ActiveRecord::Base
-  has_many :domains
+  has_many :domains, :order => :name, :dependent => :destroy
+  has_many :users, :order => :name, :dependent => :destroy
+  has_many :addresses, :dependent => :delete_all
+  has_many :note_pads, :dependent => :destroy
+  has_many :timesheet_entries, :dependent => :delete_all
   validates_presence_of :name
 end
