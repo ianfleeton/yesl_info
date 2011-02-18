f4ee0927da6ce7d79ce9234710cd776274ec3b2a
Ian Fleeton <ianfleeton@gmail.com>
2011-02-18 17:06:02 +0000

    Update to Rails 3.0.4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YeslInfo::Application.config.secret_token = '0193693f7d655cb6732d5a668fa8cfed9e754056eb87037dcce3ca05200dd2c99cdea5f165d333e18c4147fc2cee7c0b0d6c4fa6dda391b66369cbe76cd6eeea'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_yesl_info_session',
-  :secret      => '6cc901d2a3f7c2ba6cbb894f3fdeea9a7b8179378f1bbbd5aa36b1d07b34b232ce157fc928bd0d345e353410df84808b5ddf2ccfa5d6128f2985772683599d0d'
-}
+YeslInfo::Application.config.session_store :cookie_store, :key => '_yesl_info_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# YeslInfo::Application.config.session_store :active_record_store
