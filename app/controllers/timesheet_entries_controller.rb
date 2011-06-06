8988b8323d6de1e0497d22ca530a4f04a64a9cb3
Ian Fleeton <ianfleeton@gmail.com>
2011-06-06 19:41:23 +0100

    admin_required for TimesheetEntriesController

--- app/controllers/timesheet_entries_controller.rb
+++ app/controllers/timesheet_entries_controller.rb
@@ -1,4 +1,6 @@
 class TimesheetEntriesController < ApplicationController
+  before_filter :admin_required
+
   def index
     @staff = User.staff
     @timesheet_entries = TimesheetEntry.all(:order => 'started_at DESC', :limit => 40)
