8963aff03e50c7a15324d6ee35dc2084526671e6
Ian Fleeton <ianfleeton@gmail.com>
2012-07-14 16:11:33 +0100

    Revenue per hour

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -10,4 +10,22 @@ class Organisation < ActiveRecord::Base
   scope :recently_viewed, :order => 'last_viewed_at DESC', :limit => 10
 
   validates_presence_of :name
+
+  def revenue_per_hour
+    minutes = minutes_logged
+    return 0 if 0 == minutes
+    invoice_value / (minutes / 60)
+  end
+
+  def invoice_value
+    timesheet_entries
+      .where('chargeable = 1 AND started_at >= DATE_SUB(NOW(), INTERVAL 2 YEAR)')
+      .sum('invoice_value')
+  end
+
+  def minutes_logged
+    timesheet_entries
+      .where('started_at >= DATE_SUB(NOW(), INTERVAL 2 YEAR)')
+      .sum('minutes')
+  end
 end
