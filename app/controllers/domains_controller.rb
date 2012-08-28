d0788c4b6691c28795931c0fa9948c44064d98ed
Ian Fleeton <ianfleeton@gmail.com>
2012-08-28 07:38:08 +0100

    Mass assignment attributes for domain

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,9 +1,9 @@
 class DomainsController < ApplicationController
   before_filter :admin_required
-  before_filter :find_domain, :except => [:index, :new, :create]
+  before_filter :find_domain, except: [:index, :new, :create]
 
   def index
-    @domains = Domain.all :order => :name
+    @domains = Domain.order('name')
   end
   
   def show
@@ -26,7 +26,7 @@ class DomainsController < ApplicationController
       flash[:notice] = "Added new domain."
       redirect_to domain_path(@domain)
     else
-      render :action => "new"
+      render action: 'new'
     end
   end
 
@@ -35,26 +35,27 @@ class DomainsController < ApplicationController
 
   def update
     if @domain.update_attributes(params[:domain])
-      flash[:notice] = 'Domain updated.'
-      redirect_to domain_path(@domain)
+      redirect_to @domain, notice: 'Domain updated.'
     else
-      render :action => 'edit'
+      render action: 'edit'
     end
   end
 
   def destroy
     @domain.destroy
-    flash[:notice] = "Domain deleted."
-    redirect_to organisation_path(@domain.organisation)
+    redirect_to @domain.organisation, notice: 'Domain deleted.'
+  end
+
+  def renewals
+    
   end
 
   private
   
   def find_domain
-    @domain = Domain.find_by_id(params[:id], :include => :organisation)
+    @domain = Domain.find_by_id(params[:id], include: :organisation)
     if @domain.nil?
-      flash[:notice] = "That domain doesn't exist."
-      redirect_to domains_path
+      redirect_to domains_path, notice: "That domain doesn't exist."
     end
   end
 end
--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,6 +1,8 @@
 class Domain < ActiveRecord::Base
+  attr_accessible :forwarding_id, :name, :organisation_id, :registered_on, :with_us
+
   belongs_to :organisation, touch: true
-  belongs_to :forwarding, :class_name => 'Domain'
-  has_many :forwarding_from, :class_name => 'Domain', :foreign_key => 'forwarding_id', :order => :name, :dependent => :nullify
-  has_many :hosting_accounts, :order => :host_name
+  belongs_to :forwarding, class_name: 'Domain'
+  has_many :forwarding_from, class_name: 'Domain', foreign_key: 'forwarding_id', order: 'name', dependent: :nullify
+  has_many :hosting_accounts, order: 'host_name'
 end
