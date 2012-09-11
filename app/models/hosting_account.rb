6f373fcd73d34e3d3090a7aaa48cb175464963c1
Ian Fleeton <ianfleeton@gmail.com>
2012-09-11 19:20:09 +0100

    Whitelist attributes for hosting accounts

--- app/models/hosting_account.rb
+++ app/models/hosting_account.rb
@@ -1,3 +1,6 @@
 class HostingAccount < ActiveRecord::Base
+  attr_accessible :annual_fee, :backed_up_on, :backup_cycle, :domain_id,
+    :ftp_host, :host_name, :started_on, :username, :password
+
   belongs_to :domain, touch: true
 end
