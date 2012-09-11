0e3c5fa1eb8550a28e17ad262f9559938f0d3df1
Ian Fleeton <ianfleeton@gmail.com>
2012-09-11 19:16:07 +0100

    Mini buttons (need refactoring)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -85,11 +85,25 @@ module ApplicationHelper
     class: 'btn'
   end
 
+  def mini_edit_button(object)
+    link_to '<i class="icon-edit"></i> Edit'.html_safe,
+    edit_polymorphic_path(object),
+    class: 'btn btn-mini'
+  end
+
   def delete_button(object)
     link_to '<i class="icon-trash icon-white"></i> Delete'.html_safe,
     object,
     data: { confirm: 'Are you sure?' },
     method: :delete,
     class: 'btn btn-danger'
+  end
+
+  def mini_delete_button(object)
+    link_to '<i class="icon-trash icon-white"></i> Delete'.html_safe,
+    object,
+    data: { confirm: 'Are you sure?' },
+    method: :delete,
+    class: 'btn btn-danger btn-mini'
   end
 end
