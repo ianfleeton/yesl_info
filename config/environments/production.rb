d0a2cf804bb2b30868ea793b15e97883251c20d4
Ian Fleeton <ianfleeton@gmail.com>
2013-04-16 18:53:14 +0100

    Use sendmail to avoid OpenSSL SSLError unknown protocol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,3 +78,5 @@ YeslInfo::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 end
+
+ActionMailer::Base.delivery_method = :sendmail
