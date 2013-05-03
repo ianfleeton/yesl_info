06da3083eecc24b236709c85313a04938d407ece
Ian Fleeton <ianfleeton@gmail.com>
2013-05-03 17:40:12 +0100

    Use Capistrano for deployment

--- Gemfile
+++ Gemfile
@@ -57,7 +57,8 @@ end
 # gem 'unicorn'
 
 # Use Capistrano for deployment
-# gem 'capistrano', group: :development
+gem 'capistrano', group: :development
+gem 'rvm-capistrano', group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
---
+++ config/deploy.rb
@@ -0,0 +1,45 @@
+require "bundler/capistrano" 
+
+require "rvm/capistrano"
+set :rvm_type, :system
+set :rvm_path, "/usr/local/rvm"
+
+set :application, "yesl_info"
+set :repository,  "https://ianfleeton:cpXfzATYvq@bitbucket.org/youresolutions/yesl_info.git"
+set :use_sudo, false
+
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :deploy_to, "/home/admin/sites/#{application}"
+
+server 'io.yesl.co.uk', :app, :web, :db, primary: true
+set :user, 'admin'
+default_run_options[:pty] = true
+
+set :keep_releases, 5
+# if you want to clean up old releases on each deploy uncomment this:
+after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+before 'deploy:assets:precompile', 'deploy:symlink_db'
+
+after 'deploy:update_code', 'deploy:migrate'
+
+ssh_options[:forward_agent] = true
