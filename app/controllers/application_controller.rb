373d46cdc74842992e8f0e69670ac860d18437e5
Ian Fleeton <ianfleeton@gmail.com>
2013-05-03 14:38:32 +0100

    Update to Rails 4.0.0.rc1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
+
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   # Scrub sensitive parameters from your log
