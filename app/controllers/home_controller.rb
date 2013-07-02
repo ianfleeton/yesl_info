86deffad6ca6cfbfbb7aa461c673778ed027c2b5
Ian Fleeton <ianfleeton@gmail.com>
2013-07-02 15:40:40 +0100

    Simpler password generation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -31,10 +31,7 @@ class HomeController < ApplicationController
 
   private
   
-  def create_password
-    chars = %w{a b c d e f g h j k m n o p q r s t u v w x y z A B C D E F G H J K L M N P Q R S T U V W X Y Z 2 3 4 5 6 7 8 9}
-    pw = ''
-    10.times {pw += chars[rand(chars.length)]}
-    pw
-  end
+    def create_password
+      SecureRandom.base64(9)
+    end
 end
