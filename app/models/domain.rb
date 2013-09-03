66b6c31d368b68f6fe6936c73a0fa078befcc94a
Ian Fleeton <ianfleeton@gmail.com>
2013-09-03 14:20:52 +0100

    Delete dependent hosting accounts

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -2,7 +2,7 @@ class Domain < ActiveRecord::Base
   belongs_to :organisation, touch: true
   belongs_to :forwarding, class_name: 'Domain'
   has_many :forwarding_from, -> { order 'name' }, class_name: 'Domain', foreign_key: 'forwarding_id', dependent: :nullify
-  has_many :hosting_accounts, -> { order 'host_name' }
+  has_many :hosting_accounts, -> { order 'host_name' }, dependent: :delete_all
 
   validates_presence_of :name
   validates_presence_of :organisation
