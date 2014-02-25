8fa00463dad3641855fe259e9f4b171ea19b2415
Ian Fleeton <ianfleeton@gmail.com>
2014-02-25 13:52:45 +0000

    Cache organisation for show_admin

--- app/models/organisation_watcher.rb
+++ app/models/organisation_watcher.rb
@@ -1,5 +1,5 @@
 class OrganisationWatcher < ActiveRecord::Base
-  belongs_to :organisation
+  belongs_to :organisation, touch: true
   belongs_to :watcher, class_name: 'User', foreign_key: 'user_id'
 
   validates :user_id, uniqueness: { scope: :organisation_id }
