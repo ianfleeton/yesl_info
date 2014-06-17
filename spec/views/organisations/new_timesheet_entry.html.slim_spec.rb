76504fc1fa22a36dd8871456ab49a573b33ffbe0
Ian Fleeton <ianfleeton@gmail.com>
2014-06-17 09:19:31 +0100

    Supply @organisation

--- spec/views/organisations/new_timesheet_entry.html.slim_spec.rb
+++ spec/views/organisations/new_timesheet_entry.html.slim_spec.rb
@@ -4,6 +4,7 @@ describe 'organisations/new_timesheet_entry.html.slim' do
   let(:timesheet_entry) { TimesheetEntry.new }
 
   before do
+    assign(:organisation, Organisation.new)
     assign(:timesheet_entry, timesheet_entry)
   end
 
