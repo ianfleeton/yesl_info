59832c868457b97b7d10c947515e36feb3fd9979
Ian Fleeton <ianfleeton@gmail.com>
2014-06-17 10:14:23 +0100

    Update Cap files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,33 +1,37 @@
 # Simple Role Syntax
 # ==================
-# Supports bulk-adding hosts to roles, the primary
-# server in each group is considered to be the first
-# unless any hosts have the primary property set.
-# Don't declare `role :all`, it's a meta role
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
 role :app, %w{admin@io.yesl.co.uk}
 role :web, %w{admin@io.yesl.co.uk}
 role :db,  %w{admin@io.yesl.co.uk}
 
 # Extended Server Syntax
 # ======================
-# This can be used to drop a more detailed server
-# definition into the server list. The second argument
-# something that quacks like a hash can be used to set
-# extended properties on the server.
+# This can be used to drop a more detailed server definition into the
+# server list. The second argument is a, or duck-types, Hash and is
+# used to set extended properties on the server.
 server 'io.yesl.co.uk', user: 'admin', roles: %w{web app db}, my_property: :my_value
 
 set :branch, ENV['REVISION'] || ENV['BRANCH_NAME'] || 'master'
 
-# you can set custom ssh options
-# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
-# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
-# set it globally
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+#
+# Global options
+# --------------
 #  set :ssh_options, {
 #    keys: %w(/home/rlisowski/.ssh/id_rsa),
 #    forward_agent: false,
 #    auth_methods: %w(password)
 #  }
-# and/or per server
+#
+# And/or per server (overrides global)
+# ------------------------------------
 # server 'example.com',
 #   user: 'user_name',
 #   roles: %w{web app},
@@ -38,4 +42,3 @@ set :branch, ENV['REVISION'] || ENV['BRANCH_NAME'] || 'master'
 #     auth_methods: %w(publickey password)
 #     # password: 'please use keys'
 #   }
-# setting per server overrides global ssh_options
