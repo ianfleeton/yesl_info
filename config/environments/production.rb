01893c340cb5dd410dea3eefcb51b3f8efe38e6c
Ian Fleeton <ianfleeton@gmail.com>
2014-12-17 14:01:15 +0000

    Secrets isn't around until later (after initialize)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,16 +65,18 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              Secrets[:smtp_address],
-    port:                 Secrets[:smtp_port],
-    enable_starttls_auto: Secrets[:smtp_enable_starttls_auto],
-    domain:               'yesl.info',
-    user_name:            Secrets[:smtp_user_name],
-    password:             Secrets[:smtp_password],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
-
+  config.after_initialize do
+    config.action_mailer.smtp_settings = {
+      address:              Secrets[:smtp_address],
+      port:                 Secrets[:smtp_port],
+      enable_starttls_auto: Secrets[:smtp_enable_starttls_auto],
+      domain:               'yesl.info',
+      user_name:            Secrets[:smtp_user_name],
+      password:             Secrets[:smtp_password],
+      authentication:       'plain',
+      enable_starttls_auto: true  }
+  end
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
