6add308aa646c2c03a68eabeec19a67d5b25932d
Dylan Renwick <dylanr@yesl.co.uk>
2015-08-24 10:28:44 +0100

    Set non-nil values

--- spec/models/timesheet_entry_spec.rb
+++ spec/models/timesheet_entry_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe TimesheetEntry, type: :model do
+  describe "before_validation :set_non_nil_values" do
+    it "sets non-nil values" do
+      timesheet = TimesheetEntry.new(invoice_value: nil, minutes: nil)
+      timesheet.valid?
+      expect(timesheet.invoice_value).to eq 0
+      expect(timesheet.minutes).to eq 0
+    end
+  end
 end
