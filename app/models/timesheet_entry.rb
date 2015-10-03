55d9c664ffccca25be68da179c70d6ae52306b80
Ian Fleeton <ianfleeton@gmail.com>
2015-10-03 22:20:15 +0100

    Remove hard-coded hack -- can come back when fixed

--- app/models/timesheet_entry.rb
+++ app/models/timesheet_entry.rb
@@ -6,13 +6,11 @@ class TimesheetEntry < ActiveRecord::Base
   before_validation :set_non_nil_values
 
   def self.recently_worked_with
-    # TODO: there should be an attribute of organisations instead of this hacky list of IDs to exclude
-    excluded_organisations = '9, 539, 562, 679, 680, 689'
     minimum_minutes = 240
     active_since_months = 12
     select('organisation_id, SUM(minutes)/60 AS hours, SUM(CASE WHEN chargeable = 1 THEN invoice_value ELSE 0 END) / (SUM(minutes) / 60) AS hourly, organisations.name')
       .joins('LEFT JOIN organisations ON organisations.id = organisation_id')
-      .where("started_at >= DATE_SUB(NOW(), INTERVAL ? MONTH) AND organisation_id NOT IN (#{excluded_organisations})", active_since_months)
+      .where("started_at >= DATE_SUB(NOW(), INTERVAL ? MONTH)", active_since_months)
       .group('organisation_id')
       .having('SUM(minutes) >= ?', minimum_minutes)
       .order('hourly DESC')
